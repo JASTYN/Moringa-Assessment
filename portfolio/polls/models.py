af85dae492fc2cdc0c278e4a8baeaa2df7391167
JASTYN <jastynmanya@gmail.com>
2022-04-07 15:19:18 +0300

    new

--- portfolio/polls/models.py
+++ portfolio/polls/models.py
@@ -19,5 +19,5 @@ class Choice(models.Model):
     question = models.ForeignKey('Question',on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-     def __str__(self):             
+    def __str__(self):             
         return self.choice_text
