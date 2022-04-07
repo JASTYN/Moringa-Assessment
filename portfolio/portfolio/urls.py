548522a10f3618ce63b97b753efa2017ab5a6a53
JASTYN <jastynmanya@gmail.com>
2022-04-07 14:57:04 +0300

    new

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -1,4 +1,4 @@
-"""myportfolio URL Configuration
+"""portfolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
